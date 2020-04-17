fa9aebd36c9f61d8653f7dc2b1c440d808b74dae
Eric Priou @ hetic <eric@flaure.fr>
2020-04-17 22:37:47 +0200

    [new] First architecture

---
+++ src/components/Log.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import AuthContext from "../context/AuthContext";
+
+const styles =
+{
+  green : { backgroundColor : "green" }
+  ,red : { backgroundColor : "red" }
+}
+
+export default () =>
+{
+  const { isAuth, setAuth } = React.useContext( AuthContext );
+
+  return(
+    <div
+      style={ isAuth ? styles.green : styles.red }
+    >
+      { isAuth ? "OK" : "KO" }
+      <hr/>
+      <button onClick={ e => setAuth( ! isAuth )  }>CHANGE ME</button>
+    </div>
+  );
+}
---
+++ src/components/LogContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Log from "./Log";
+
+export default props =>
+{
+  return(
+    <>
+      <h1>Authentifiez-vous</h1>
+      <Log/>
+    </>
+  );
+}
